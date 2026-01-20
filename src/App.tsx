a529c35d1b75071520e9a979b40ac9a0b69bc46b
Palmira <palmira.r@gmail.com>
2026-01-19 23:51:05 -0600

    Respaldo antes de pruebas de registro de usuario

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import Dashboard from './pages/Dashboard'
 import './App.css'
 import { Routes, Route } from 'react-router-dom'
 import Home from './pages/Home';
+import Register from './pages/Register';
 
 function App() {
 
@@ -9,6 +10,7 @@ function App() {
     <Routes>
       <Route path="/" element={<Home />} />
       <Route path="/dashboard" element={<Dashboard />} />
+      <Route path="/register" element={<Register />} />
     </Routes>
   );
 }
